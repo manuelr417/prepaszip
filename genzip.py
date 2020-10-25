45f2d2416b0fe9ad9cb97cb4097679f1f8fa0910
manuelrodriguez <manuel.rodriguez@upr.edu>
2020-10-25 10:52:35 -0400

    Fixing

--- genzip.py
+++ genzip.py
@@ -5,14 +5,19 @@ destination_dir = "/home/ubuntu/zips/UAGM"
 def generate_zip(input_csv_path, certifactes_path, output_dir_path):
     with open(input_csv_path) as csvfile:
         csvreader =csv.reader(csvfile)
+        i = 0
         for row in csvreader:
-            print(row[0])
-            fname = certifactes_path + "/GP-"+row[0]+".pdf"
-            outname = output_dir_path+ "/GP-"+row[0]+".pdf"
-            print(fname)
-            print(outname)
-            shutil.copyfile(fname, outname)
-            print("copied")
+            if i == 0:
+                pass
+            else:
+                print(row[0])
+                fname = certifactes_path + "/GP-"+row[0]+".pdf"
+                outname = output_dir_path+ "/GP-"+row[0]+".pdf"
+                print(fname)
+                print(outname)
+                shutil.copyfile(fname, outname)
+                print("copied")
+            i = i + 1
 
 if __name__== "__main__":
     generate_zip("UAGM.csv", source_dir, destination_dir)
