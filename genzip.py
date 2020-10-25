9786c44ce98542dae5e17840dd3752184666f955
manuelrodriguez <manuel.rodriguez@upr.edu>
2020-10-25 10:58:32 -0400

    Fixing

--- genzip.py
+++ genzip.py
@@ -1,7 +1,7 @@
 import csv
 import shutil
 source_dir = "/home/ubuntu/git/PrepasGP/internal/cert"
-destination_dir = "/home/ubuntu/zips/UAGM"
+destination_dir = "/home/ubuntu/zips/UPR"
 def generate_zip(input_csv_path, certifactes_path, output_dir_path):
     with open(input_csv_path) as csvfile:
         csvreader =csv.reader(csvfile)
@@ -16,4 +16,4 @@ def generate_zip(input_csv_path, certifactes_path, output_dir_path):
             print("copied")
 
 if __name__== "__main__":
-    generate_zip("UAGM.csv", source_dir, destination_dir)
+    generate_zip("UPR.csv", source_dir, destination_dir)
