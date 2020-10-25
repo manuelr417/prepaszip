b39b0e108420858017393eeff5ec8019d71801f4
manuelrodriguez <manuel.rodriguez@upr.edu>
2020-10-25 11:03:50 -0400

    Fixing

--- genzip.py
+++ genzip.py
@@ -4,16 +4,23 @@ source_dir = "/home/ubuntu/git/PrepasGP/internal/cert"
 destination_dir = "/home/ubuntu/zips/UPR"
 def generate_zip(input_csv_path, certifactes_path, output_dir_path):
     with open(input_csv_path) as csvfile:
+
         csvreader =csv.reader(csvfile)
         i = 0
+        text_file = open("Error.txt", "w")
         for row in csvreader:
             print(row[0])
             fname = certifactes_path + "/GP-"+row[0]+".pdf"
             outname = output_dir_path+ "/GP-"+row[0]+".pdf"
             print(fname)
             print(outname)
-            shutil.copyfile(fname, outname)
+            try:
+                shutil.copyfile(fname, outname)
+            except:
+                print(f"GP Not found: {outname}", file=text_file)
             print("copied")
+        text_file.flush()
+        text_file.close
 
 if __name__== "__main__":
     generate_zip("UPR.csv", source_dir, destination_dir)
